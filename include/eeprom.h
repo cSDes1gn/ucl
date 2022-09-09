3a9bb881c64e318bbbbd5f30f9a75591c77f4724
ztnel <christian911@sympatico.ca>
2022-09-09 11:49:55 -0400

    add guards and fix enable bit checks

--- include/eeprom.h
+++ include/eeprom.h
@@ -5,8 +5,10 @@
 
 typedef int eeprom_status_t;
 
-#define EEPROM_SUCCESS (eeprom_status_t)0;
-#define EEPROM_FAILURE (eeprom_status_t)1;
+#define EEPROM_SUCCESS (eeprom_status_t)0
+#define EEPROM_FAILURE (eeprom_status_t)1
+
+#define EEPROM_MAX_ADDR 4096
 
 eeprom_status_t eeprom_read_byte(uint16_t addr, uint8_t *data);
 eeprom_status_t eeprom_write_byte(uint16_t addr, uint8_t data);
--- src/eeprom.c
+++ src/eeprom.c
@@ -6,14 +6,17 @@
  * @brief Reads a byte from EEPROM at specified address and stores it in the 
  * passed data buffer. Disables global interrupt mask during transaction.
  * 
- * @param addr EEPROM read address
+ * @param addr EEPROM read addres. Must be in the range [0, EEPROM_MAX_ADDR]
  * @param data data buffer for EEPROM byte at read address
  * @return eeprom_status_t 
  */
 eeprom_status_t eeprom_read_byte(uint16_t addr, uint8_t *data) {
+  if (addr > 4096) {
+    return EEPROM_FAILURE;
+  }
   /* Wait for previous EEPROM transaction
      NOTE: EEPE is self-cleared by hardware after EEPROM write transactions */
-  while (EECR & (1 << EEPE) == 0);
+  while (EECR & (1 << EEPE));
   cli();
   EEAR = addr;
   // start read transaction
@@ -28,17 +31,20 @@ eeprom_status_t eeprom_read_byte(uint16_t addr, uint8_t *data) {
  * @brief Writes a byte to EEPROM at specified address. Disables global
  * interrupt mask during transaction.
  * 
- * @param addr eeprom write address
+ * @param addr eeprom write address. Must be in the range [0, EEPROM_MAX_ADDR]
  * @param data eeprom write byte
  * @return eeprom_status_t 
  */
 eeprom_status_t eeprom_write_byte(uint16_t addr, uint8_t data) {
+  if (addr > 4096) {
+    return EEPROM_FAILURE;
+  }
   /* Wait for previous EEPROM transaction
      NOTE: EEPE is self-cleared by hardware after EEPROM write transactions */
-  while (EECR & (1 << EEPE) == 0);
+  while (EECR & (1 << EEPE));
   /* relevant if the software contains a Boot Loader allowing the CPU to program the Flash
      wait for store program memory instruction */
-  while (SPMCSR & (1 << SPMEN) == 0);
+  while (SPMCSR & (1 << SPMEN));
   cli();
   // write to EEPROM address and data registers 
   EEAR = addr;
