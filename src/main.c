a6c32d7f62c99ab558c71a0ca7ffe3668d2e6f55
ztnel <christian911@sympatico.ca>
2022-09-09 11:50:08 -0400

    add eeprom test

--- src/main.c
+++ src/main.c
@@ -1,15 +1,40 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+
 #include "usart.h"
+#include "eeprom.h"
+
+void eeprom_test(void) {
+  uint8_t buffer = 0;
+  eeprom_status_t result = EEPROM_SUCCESS;
+  for (int i = 0; i <= EEPROM_MAX_ADDR; i++) {
+    result = eeprom_write_byte(i, (uint8_t)i);
+    if (result == EEPROM_FAILURE) {
+      usart_blocking_send('W');
+      continue;
+    }
+    _delay_ms(200);
+    result = eeprom_read_byte(i, &buffer);
+    if (result == EEPROM_FAILURE) {
+      usart_blocking_send('R');
+      continue;
+    }
+    usart_blocking_send(buffer);
+    usart_blocking_send('\r');
+    usart_blocking_send('\n');
+    _delay_ms(200);
+  }
+}
 
 int main(void){
   usart_init();
+  // eeprom_test();
   // SCK of PORTB for outpu
   DDRB |= (1 << DDB1);
   for(;;){
+    eeprom_test();
     PORTB = PORTB ^ (1 << PB1);
-    _delay_ms(100);
   }
   return 0;
 }
