6efb9dac8722b1cb3032d25abbfb7336ee9f72b0
ztnel <christian911@sympatico.ca>
2022-10-17 23:30:02 -0400

    remove test methods and complex isr debouncing

--- include/encoder.h
+++ include/encoder.h
@@ -9,13 +9,13 @@
 #define ENCODER_EVENT_BUF_LEN 100
 
 enum encoder_event {
-  ENCODER_CCW,
+  ENCODER_INVALID,
   ENCODER_CW,
-  ENCODER_SW
+  ENCODER_CCW,
+  ENCODER_SW,
 };
 
 
 enum encoder_event encoder_next_event(void);
 void encoder_init(void);
-void encoder_wake(void);
-void encoder_sleep(void);
+
